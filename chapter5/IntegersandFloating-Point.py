3531e76e2accb8dbd5df14207d7114a7d0e143d7
Abbos Pardayev <abbospardayev080@gmail.com>
2025-11-12 11:56:47 +0500

    chapter 5

---
+++ chapter5/IntegersandFloating-Point.py
@@ -0,0 +1,63 @@
+#displays the first number raised to the power of the second number.
+
+num1 = float(input("Enter base: "))
+num2 = float(input("Enter exponent: "))
+
+powerof=num1**num2
+to_intger=int(sum)
+print(f"{num1} power of {num2} is: {powerof}")
+
+#round()
+a = float(input("Enter a number: "))
+r=round(a,2)
+print(r)
+n = 7.12567890
+print(f"The value of n is {n:.2f}")
+print(f"The value of n is {n:,}")
+m=1234.56
+print(f"The value of n is {m:,.2f}")
+
+# abs
+b = float(input("Enter a number: "))
+a8s=abs(b)
+print(a8s)
+
+#is_intager
+n1 = float(input("Enter a number: "))
+n2 = float(input("Enter antoher number: "))
+
+sum = n1-n2
+i=sum.is_integer()
+print(f"The difference between {n1} and {n2} is an integer? {i} ")
+
+# Review Exercises
+x=pow(3 , .125)
+print(f"The value of n is {x:.3f}")
+
+p=150000
+print(f"Price: {p:,.2f}")
+
+per=2/10
+h=per*100
+print(f"{int((h))}%")
+print(f"{int((2/10)*100)}%")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
